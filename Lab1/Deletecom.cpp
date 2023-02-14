218b75c7ae1ab8c634b484e33df7ed19f6c6b88a
ZkunCry <aksentev.03@mail.ru>
2023-02-14 21:15:44 +0500

    delete two evenets

--- Lab1/Deletecom.cpp
+++ Lab1/Deletecom.cpp
@@ -9,8 +9,6 @@ enum class State
     Normal,
     Slash,
     Comment,
-    CarriageReturn,
-    Tabulation,
     EndComment,
     Double_Quotes,
     Odinari_Quotes
@@ -21,77 +19,71 @@ enum class Literals
     TYPE_OdinaryQuote = '\''
 };
 
-static void FileOpenCheck(ifstream &SourceFile)
+static bool FileOpenCheck(ifstream &SourceFile)
 {
     if (SourceFile.is_open())
         cout << "File is open\n";
     else
+    {
         cout << "File is not open. Incorrect name or does not exist\n";
+        return 1;
+    }
+    return 1;
 }
 int main()
 {
     State states = State::Normal;
     ifstream SourceFile("Source.c");
-    FileOpenCheck(SourceFile);
     ofstream OutFile("Out.c");
     char c;
-    
-    while ((int)(c = SourceFile.get())!=EOF)
+    if (FileOpenCheck(SourceFile))
     {
-        switch (states)
+        while ((int)(c = SourceFile.get()) != EOF)
         {
-        case State::Normal:
-            switch (c)
+            switch (states)
             {
-            case '/':
-                states = State::Slash;
+            case State::Normal:
+                switch (c)
+                {
+                case '/':
+                    states = State::Slash;
+                    break;
+                case (char)Literals::TYPE_Quote:
+                    states = State::Double_Quotes;
+                    OutFile << c;
+                    break;
+                case (char)Literals::TYPE_OdinaryQuote:
+                    states = State::Odinari_Quotes;
+                    OutFile << c;
+                    break;
+                default:
+                    OutFile << c;
+                    break;
+                }
                 break;
-            case (char)Literals::TYPE_Quote:
-                states = State::Double_Quotes;
-                OutFile << c;
+            case State::Slash:
+                if (c == '*')
+                    states = State::Comment;
+                else
+                {
+                    states = State::Normal;
+                    OutFile << '/';
+                    OutFile << c;
+                }
                 break;
-            case (char)Literals::TYPE_OdinaryQuote:
-                states = State::Odinari_Quotes;
-                OutFile << c;
-                break;
-            default:
-                OutFile << c;
-                break;
-            }
-            break;
-        case State::Slash:
-            if (c == '*')
-                states = State::Comment;
-            else
-            {
-                states = State::Normal;
-                OutFile << '/';
-                OutFile << c;
-            }
-            break;
 
-        case State::Comment:
-            if (c == '*')
-                states = State::EndComment;
-            break;
+            case State::Comment:
+                if (c == '*')
+                    states = State::EndComment;
+                break;
 
-        case State::EndComment:
-            if (c == '/')
-                states = State::Normal;
-            break;
-
-        case State::CarriageReturn:
-           
-            OutFile << c;
-            states = State::Normal;
-            break;
-        case State::Tabulation:
-            OutFile << c;
-            states = State::Normal;
-            break;
-        case State::Double_Quotes:
-            switch (c)
-            {
+            case State::EndComment:
+                if (c == '/')
+                    states = State::Normal;
+                break;
+            case State::Double_Quotes:
+                switch (c)
+                {
                 case (char)Literals::TYPE_Quote:
                     states = State::Normal;
                     OutFile << c;
@@ -99,21 +91,22 @@ int main()
                 default:
                     OutFile << c;
                     break;
-            }
-            break;
+                }
+                break;
 
-        case State::Odinari_Quotes:
-            switch (c)
-            {
-            case (char)Literals::TYPE_OdinaryQuote:
-                states = State::Normal;
-                OutFile << c;
-                break;
-            default:
-                OutFile << c;
+            case State::Odinari_Quotes:
+                switch (c)
+                {
+                case (char)Literals::TYPE_OdinaryQuote:
+                    states = State::Normal;
+                    OutFile << c;
+                    break;
+                default:
+                    OutFile << c;
+                    break;
+                }
                 break;
             }
-            break;
         }
     }
     SourceFile.close();
