f7698f6bb7725df48ac5a60481ed17eb3bae2e6e
ZkunCry <aksentev.03@mail.ru>
2023-05-08 11:43:48 +0500

    add created triads

--- Lab6/lab6.cpp
+++ Lab6/lab6.cpp
@@ -2,9 +2,12 @@
 #include <vector>
 #include <string>
 #include <fstream>
-#include <stack>
 
+constexpr short int  MATRIX_SIZE = 16;
 
+std::ifstream in;
+std::ofstream out;
+int IdTriad = 0;
 /*
 * Исходная грамматика
       S → I=E;
@@ -26,26 +29,117 @@
       M → ~M | (E) | I | C
 */
 
+class Base
+{
+public:
+    virtual std::string GetOperand() { return ""; };
+    virtual ~Base();
 
+};
 
-constexpr short int  MATRIX_SIZE = 16;
+struct Var :public Base // Struct of identifier
+{
+private:
+    std::string IdName;
+public:
+    Var(const std::string& src);
+    std::string GetOperand()override;
+};
 
-std::ifstream in;
-std::ofstream out;
-std::ofstream outoptimization;
-int IdTriad = 0;
+struct Constant :public Base
+{
+public:
+    long ConstValue;
+    Constant(const long value);
+    std::string GetOperand()override;
+};
+
+struct Reference :public Base
+{
+public:
+    long TriadRef;
+    Reference(const long TriadRef);
+    std::string GetOperand()override;
+
+};
+
+class Triad
+{
+public:
+    char operation;
+    Base* op1;
+    Base* op2;
+    bool isDelete = false;
+public:
+    Triad(char operation, Base* op1, Base* op2);
+    void OutTriad();
+
+};
+Var::Var(const std::string& src) :IdName{ src } {}
+
+std::string Var::GetOperand()
+{
+    return this->IdName;
+}
+
+Reference::Reference(const long TriadRef)
+{
+    this->TriadRef = TriadRef;
+}
+
+std::string Reference::GetOperand()
+{
+    return '^' + std::to_string(this->TriadRef);
+}
+
+struct None :public Base
+{
+public:
+    None();
+    std::string GetOperand()override;
+};
+
+Constant::Constant(const long value)
+{
+    this->ConstValue = value;
+}
+
+std::string Constant::GetOperand()
+{
+    return std::to_string(this->ConstValue);
+}
+
+Triad::Triad(char operation, Base* op1, Base* op2) :op1(op1), op2(op2), operation{ operation }
+{}
+
+void Triad::OutTriad()
+{
+    out << operation << '(' << op1->GetOperand() << ", " << op2->GetOperand() << ')' << std::endl;
+}
+
+None::None() {  }
+
+std::string None::GetOperand()
+{
+    return std::string(1, '@');
+}
+
+Base::~Base()
+{
+}
+
 
 char Matrix[MATRIX_SIZE][MATRIX_SIZE] =
 {
     {' ','L', 'S', 'E', 'T', 'M', '=', ';', '|', '&', '~', '(', ')', 'I', 'C','#'},
     {'L',' ', '=', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '<',' ','=' },
-    {'S',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',' ','>'},
-    {'E',' ', ' ', ' ', ' ', ' ', ' ', '=', '=', ' ', ' ', ' ', '=', ' ',' ',' '},
-    {'T',' ', ' ', ' ', ' ', ' ', ' ', '>', '>', '=', ' ', ' ', '>', ' ',' ',' '},
-    {'M',' ', ' ', ' ', ' ', ' ', ' ', '>', '>', '>', ' ', ' ', '>', ' ',' ',' '},
+    {'S',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',' ', '>'},
+    {'E',' ', ' ', ' ', ' ', ' ', ' ', '=', '=', ' ', ' ', ' ', '=', ' ',' ', ' '},
+    {'T',' ', ' ', ' ', ' ', ' ', ' ', '>', '>', '=', ' ', ' ', '>', ' ',' ', ' '},
+    {'M',' ', ' ', ' ', ' ', ' ', ' ', '>', '>', '>', ' ', ' ', '>', ' ',' ', ' '},
     {'=',' ', ' ', '$', '<', '<', ' ', ' ', ' ', ' ', '<', '<', ' ', '<','<', ' '},
     {';',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',' ', '>'},
-    {'|',' ', ' ', ' ', '$', '<', ' ', ' ', ' ', ' ', '<', '<', ' ', '<', '<', ' '},
+    {'|',' ', ' ', ' ', '$', '<', ' ', ' ', ' ', ' ', '<', '<', ' ', '<', '<',' '},
     {'&',' ', ' ', ' ', ' ', '=', ' ', ' ', ' ', ' ', '<', '<', ' ', '<', '<',' '},
     {'~',' ', ' ', ' ', ' ', '=', ' ', ' ', ' ', ' ', '<', '<', ' ', '<', '<',' '},
     {'(',' ', ' ', '$', '<', '<', ' ', ' ', ' ', ' ', '<', '<', ' ', '<', '<',' '},
@@ -63,6 +157,7 @@ struct Rule
     std::string right;
     Rule(char currentS, const std::string src);
 };
+
 enum class TypeErrors //Error codes
 {
     SYNTAX_ERROR,
@@ -78,6 +173,9 @@ struct Attitude
 public:
     char SymbolAttitude;
     char Symbol;
+    int ValueConstant;
+    std::string nameIdentifier;
+    int _idTriad;
 public:
     Attitude() { }
     Attitude(const char currentAttitude,const char symbol):SymbolAttitude(currentAttitude),
@@ -90,8 +188,15 @@ private:
 
     int _Lex = EOF;
     int _currentSymbol;
+    
+    std::string name;
+    int value;
+
     const std::string _Native = "=;~|&()";
-    std::vector<Attitude> stack;    
+    std::vector<Attitude> stack;
+    std::vector<Var> varlist;
+    std::vector<Triad> TriadList; 
+
     const Rule Rules[12] =
     {
         {'W',"#L#"},
@@ -107,6 +212,7 @@ private:
         {'M',"I"},
         {'M',"C"}
     };
+    
     const int rulesCount = sizeof(Rules) / sizeof(*Rules) - 1;
 
 public:
@@ -119,6 +225,8 @@ public:
     void Analize();
     char RuleConvolution();
     int FindAttitude(const char symbol);
+    bool GetVarValue(std::string& str); //Return identifier value
+    bool GetVarAdress(std::string name); //Return  identifier address or add in array
     
 };
 
@@ -158,17 +266,30 @@ inline void Translation::GetSymbol()
 
      if (isalpha(_currentSymbol) || _currentSymbol == '_')
      {
+         std::string temp;
          GetSymbol();
          while (isdigit(_currentSymbol))
+         {
+             temp += _currentSymbol;
              GetSymbol();
+
+         }
+         name = temp;
         _Lex = 'I';
+     
      }
      else if (isdigit(_currentSymbol))
      {
-     GetSymbol();
-     while (isdigit(_currentSymbol))
+         long x = 0;
          GetSymbol();
-     _Lex = 'C';
+         while (_currentSymbol >='0' && _currentSymbol <='1')
+         {
+             x *= 2;
+             x += _currentSymbol - '0';
+             GetSymbol();
+         }
+         value = x;
+         _Lex = 'C';
      }
      else if (strchr(_Native.c_str(), _currentSymbol))
      {
@@ -219,7 +340,29 @@ inline void Translation::GetSymbol()
      if (symbol == ' ')
          PrintError(TypeErrors::UNKOWN_SYMBOL, "No relation for lexeme " + symbol);
      else if (symbol != '>')
-         stack.push_back(Attitude(symbol, _Lex));
+     {
+         Attitude attitude(symbol, _Lex);
+         if (_Lex == 'C')
+         {
+             attitude.ValueConstant = value;
+             Triad triad('C', new Var(name), new None());
+             TriadList.push_back(triad);
+             IdTriad++;
+
+             attitude._idTriad = IdTriad;
+         }
+         else if (_Lex == 'I')
+         {
+             attitude.nameIdentifier = name;
+             
+             Triad triad('V', new Var(name), new None());
+             TriadList.push_back(triad);
+             IdTriad++;
+             attitude._idTriad = IdTriad;
+         }
+         stack.push_back(attitude);
+     }
+         
      else
      {
          while (symbol == '>')
@@ -253,8 +396,10 @@ inline char Translation::FindRules(std::string src)
      while (true)
      {
          tempposL = posL;
+
          for(;posL< _sizestack;posL++)
             base.push_back(stack[posL].Symbol);
+         GetVarValue(name);
          result = FindRules(base);
          posL = tempposL;
          if (result != NULL)
@@ -279,16 +424,41 @@ inline char Translation::FindRules(std::string src)
  }
  inline int Translation::FindAttitude(const char symbol)
  {
-     for (int j = stack.size() - 1; j > 0; j--)
+     for (int j = stack.size() - 1; j > 1; j--)
          if (stack[j].SymbolAttitude == symbol)
              return j;
+ }
+
+ bool Translation::GetVarValue(std::string& str)
+ {
+     if (str.size() == 0 || str.empty())
+         PrintError(TypeErrors::SYNTAX_ERROR);
+
+     for (int i = 0; i < varlist.size(); i++)
+         if (varlist[i].GetOperand() == str)
+             return true;
+
+     PrintError(TypeErrors::UNDEF_ID, str);
+   
+ }
+
+ bool Translation::GetVarAdress(std::string name)
+ {
+     if (name.empty() || name.size() == 0)
+         PrintError(TypeErrors::SYNTAX_ERROR);
+
+     for (int i = 0; i < varlist.size(); i++)
+         if (varlist[i].GetOperand() == name)
+             return true;
+     varlist.push_back(Var{ name });
+ 
  }
 
  int main()
  {
      in.open("text.txt", std::ios::binary);
-
-     if (!in.is_open())
+     out.open("translation.txt", std::ios::binary);
+     if (!in.is_open() || !out.is_open())
      {
          std::cout << "Error! Can't open file " << std::endl;
          return 2;
@@ -298,7 +468,7 @@ inline char Translation::FindRules(std::string src)
      start.Run();
      in.close();
      out.close();
-     outoptimization.close();
+
      return 0;
  }
 
